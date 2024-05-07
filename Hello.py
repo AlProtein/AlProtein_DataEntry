b8522525af08e905d9d0e6d19c9c86a3937a2b1b
Ramy Ali <99515327+ramyalii@users.noreply.github.com>
2024-05-07 09:18:39 +0000

    update

--- Hello.py
+++ Hello.py
@@ -12,10 +12,11 @@ LOGGER = get_logger(__name__)
 # Function to connect to AWS RDS database
 def create_connection():
     conn = psycopg2.connect(
-        database="database-1",
-        user="postgres",
-        password="4R5KsTF0AOYO3FkAZUJc",
-        host="database-1.cluster-cc4idrhev5xz.eu-north-1.rds.amazonaws.com",
+        database="dataacquisition",
+        # concentration database
+        user="alpro",
+        password="2Os24AFzLL5csQiK3rDu",
+        host="dataacquisition.cc4idrhev5xz.eu-north-1.rds.amazonaws.com",
         port="5432")
     return conn
 
